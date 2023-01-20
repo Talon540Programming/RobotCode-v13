a0983464660fe75edcf2db0734c9ff76c09c18da
LefflerC <114716938+LefflerC@users.noreply.github.com>
2023-01-20 18:16:32 -0500

    OperatorMode Conguring and XBox Controller
    Copy pasted operator modes, added some statements that only work for the drivetrain if you have the xbox, attempted to just add an xbox controller by just copy pasting the declaration and initialization from 14.0 but I don't know if that actually works.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import frc.robot.shooter.commands.control.AttackJoystickShooterControl;
 public class RobotContainer {
   private final CommandJoystick leftJoystick = new CommandJoystick(RobotMap.kLeftJoystickPort);
   private final CommandJoystick rightJoystick = new CommandJoystick(RobotMap.kRightJoystickPort);
+  private final TalonXboxController xboxController = new TalonXboxController(2);
 
   // Subsystems
   private final DrivetrainBase drivetrainBase = new DrivetrainBase();
@@ -26,11 +27,38 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    drivetrainBase.setDefaultCommand(
-        new AttackJoystickDriveControl(drivetrainBase, leftJoystick, rightJoystick));
-    shooterBase.setDefaultCommand(
-        new AttackJoystickShooterControl(shooterBase, leftJoystick, rightJoystick));
-    intakeBase.setDefaultCommand(
-        new AttackJoystickIntakeControl(intakeBase, leftJoystick, rightJoystick));
+    switch(operatorMode) {
+      case ATTACK_ONLY:
+        drivetrainBase.setDefaultCommand(
+            new AttackJoystickDriveControl(drivetrainBase, leftJoystick, rightJoystick));
+        shooterBase.setDefaultCommand(
+            new AttackJoystickShooterControl(shooterBase, leftJoystick, rightJoystick));
+        intakeBase.setDefaultCommand(
+            new AttackJoystickIntakeControl(intakeBase, leftJoystick, rightJoystick));
+        break;
+      case XBOX_ONLY: 
+        drivetrainBase.setDefaultCommand(new XboxJoystickDriveControl(drivetrainbase, xboxController))
+        break;
+      case XBOX_AND_ATTACK:
+        drivetrainBase.setDefaultCommand(new XboxJoystickDriveControl(drivetrainbase, xboxController))
+        break;
+    }
   }
+  
+  private void configureButtonBindings() {
+        boolean joystickOneConnected = DriverStation.isJoystickConnected(0);
+        boolean joystickTwoConnected = DriverStation.isJoystickConnected(1);
+        boolean xboxcontrollerConnected = DriverStation.isJoystickConnected(2);
+
+        if((joystickOneConnected && joystickTwoConnected)) {
+            if(xboxcontrollerConnected) {
+                configureButtonBindings(OperatorModes.XBOX_AND_ATTACK);
+            } else {
+                configureButtonBindings(OperatorModes.ATTACK_ONLY);
+            }
+        } else {
+            configureButtonBindings(OperatorModes.XBOX_ONLY);
+        }
+
+    }
 }
