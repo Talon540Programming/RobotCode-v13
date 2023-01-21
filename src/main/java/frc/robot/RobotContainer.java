65a8f7a7df987deaff6b22e151892f65fa2c6e6a
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-20 23:15:00 -0500

    Add multi control framework

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,18 +2,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import frc.robot.constants.RobotMap;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Constants;
+import frc.robot.constants.RobotConfig;
 import frc.robot.drivetrain.DrivetrainBase;
 import frc.robot.drivetrain.commands.control.AttackJoystickDriveControl;
+import frc.robot.drivetrain.commands.control.XboxControllerDriveControl;
 import frc.robot.intake.IntakeBase;
 import frc.robot.intake.commands.control.AttackJoystickIntakeControl;
 import frc.robot.shooter.ShooterBase;
 import frc.robot.shooter.commands.control.AttackJoystickShooterControl;
 
 public class RobotContainer {
-  private final CommandJoystick leftJoystick = new CommandJoystick(RobotMap.kLeftJoystickPort);
-  private final CommandJoystick rightJoystick = new CommandJoystick(RobotMap.kRightJoystickPort);
-  private final TalonXboxController xboxController = new TalonXboxController(2);
+  private final CommandJoystick leftJoystick = new CommandJoystick(RobotConfig.kLeftJoystickPort);
+  private final CommandJoystick rightJoystick = new CommandJoystick(RobotConfig.kRightJoystickPort);
+  private final CommandXboxController xboxController = new CommandXboxController(RobotConfig.kXboxControllerPort);
 
   // Subsystems
   private final DrivetrainBase drivetrainBase = new DrivetrainBase();
@@ -23,42 +26,21 @@ public class RobotContainer {
   public RobotContainer() {
     DriverStation.silenceJoystickConnectionWarning(true);
 
-    configureBindings();
+    configureBindings(RobotConfig.controlMode);
   }
 
-  private void configureBindings(OperatorModes operatorMode) {
+  private void configureBindings(Constants.ControlMode operatorMode) {
     switch(operatorMode) {
-      case ATTACK_ONLY:
-        drivetrainBase.setDefaultCommand(
-            new AttackJoystickDriveControl(drivetrainBase, leftJoystick, rightJoystick));
-        shooterBase.setDefaultCommand(
-            new AttackJoystickShooterControl(shooterBase, leftJoystick, rightJoystick));
-        intakeBase.setDefaultCommand(
-            new AttackJoystickIntakeControl(intakeBase, leftJoystick, rightJoystick));
-        break;
-      case XBOX_ONLY: 
-        drivetrainBase.setDefaultCommand(new XboxJoystickDriveControl(drivetrainbase, xboxController))
-        break;
-      case XBOX_AND_ATTACK:
-        drivetrainBase.setDefaultCommand(new XboxJoystickDriveControl(drivetrainbase, xboxController))
-        break;
+        case JOYSTICKS:
+            drivetrainBase.setDefaultCommand(
+                    new AttackJoystickDriveControl(drivetrainBase, leftJoystick, rightJoystick));
+            shooterBase.setDefaultCommand(
+                    new AttackJoystickShooterControl(shooterBase, leftJoystick, rightJoystick));
+            intakeBase.setDefaultCommand(
+                    new AttackJoystickIntakeControl(intakeBase, leftJoystick, rightJoystick));
+            break;
+        case XBOX:
+            break;
     }
   }
-  
-  private void configureButtonBindings() {
-        boolean joystickOneConnected = DriverStation.isJoystickConnected(0);
-        boolean joystickTwoConnected = DriverStation.isJoystickConnected(1);
-        boolean xboxcontrollerConnected = DriverStation.isJoystickConnected(2);
-
-        if((joystickOneConnected && joystickTwoConnected)) {
-            if(xboxcontrollerConnected) {
-                configureButtonBindings(OperatorModes.XBOX_AND_ATTACK);
-            } else {
-                configureButtonBindings(OperatorModes.ATTACK_ONLY);
-            }
-        } else {
-            configureButtonBindings(OperatorModes.XBOX_ONLY);
-        }
-
-    }
 }
--- src/main/java/frc/robot/drivetrain/DrivetrainBase.java
+++ src/main/java/frc/robot/drivetrain/DrivetrainBase.java
@@ -3,17 +3,17 @@ package frc.robot.drivetrain;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.RobotMap;
+import frc.robot.constants.Constants.HardwareIDs;
 
 public class DrivetrainBase extends SubsystemBase {
   private final MotorControllerGroup left;
   private final MotorControllerGroup right;
 
   public DrivetrainBase() {
-    WPI_TalonFX frontLeft = new WPI_TalonFX(RobotMap.kDrivetrainFrontLeft);
-    WPI_TalonFX frontRight = new WPI_TalonFX(RobotMap.kDrivetrainFrontRight);
-    WPI_TalonFX backLeft = new WPI_TalonFX(RobotMap.kDrivetrainBackLeft);
-    WPI_TalonFX backRight = new WPI_TalonFX(RobotMap.kDrivetrainBackRight);
+    WPI_TalonFX frontLeft = new WPI_TalonFX(HardwareIDs.kDrivetrainFrontLeft);
+    WPI_TalonFX frontRight = new WPI_TalonFX(HardwareIDs.kDrivetrainFrontRight);
+    WPI_TalonFX backLeft = new WPI_TalonFX(HardwareIDs.kDrivetrainBackLeft);
+    WPI_TalonFX backRight = new WPI_TalonFX(HardwareIDs.kDrivetrainBackRight);
 
     this.left = new MotorControllerGroup(frontLeft, backLeft);
     this.right = new MotorControllerGroup(frontRight, backRight);
